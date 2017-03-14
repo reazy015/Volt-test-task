dc81f8881c0a455179aa8feea73367d92c552ac1
JohnGrey <redshuhart015@gmail.com>
2017-03-14 18:35:06 +0300

    boilerplate for test-task

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'react-select/dist/react-select.css';
+
+ReactDOM.render(<div>Place your application here</div>, document.getElementById('app-root'));
