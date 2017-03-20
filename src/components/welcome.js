2ca64e9c6ca8aea7ee63c40fd8184afbfe05fd59
JohnGrey <redshuhart015@gmail.com>
2017-03-21 01:20:30 +0300

    styling to welcome

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Grid } from 'react-bootstrap';
 import DocumentTitle from 'react-document-title';
 
 export default () => {
   return(
 		<DocumentTitle title={'Invoice App'}>
+    <Grid>
 		<Jumbotron>
 	    <h1 style={{textAlign: 'center'}}>Welcome to Invoice App!</h1>
 	    <p>Currently Products and Customers functionalities are only available. To use one of them, please, click on appropriate item in the header of the InvoiceApp. </p>
 			<p>Invoices functionality currently in production, We apologize for the inconvenience:)</p>
 	  </Jumbotron>
+  </Grid>
 	</DocumentTitle>
 	)
 }
